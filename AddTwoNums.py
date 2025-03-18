580898f5ecd51da3b5dbc7b7db1d0a168ff75eca
nailong233 <3370683025@qq.com>
2025-03-18 20:04:12 +0800

    Update AddTwoNums.py to Version 2

--- AddTwoNums.py
+++ AddTwoNums.py
@@ -1,5 +1,5 @@
-# Add Two Numbers in Python
-# Author: Your Name here # Using the + Operator
-a = 15 b = 12 
-# Adding two numbers 
-res = a + b print(res)
+# AddTwoNums.py (Version 2)
+num1 = float(input("请输入第一个数字: "))
+num2 = float(input("请输入第二个数字: "))
+result = num1 + num2
+print(f"两数之和为: {result}")
